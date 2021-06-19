c99984634d13a1c0350d1795d3bc53d35a50e421
n1c0201 <nicarthur21@gmail.com>
2021-06-19 20:53:00 +0700

    initial commit, created server.py and client handling function

---
+++ Server.py
@@ -0,0 +1,26 @@
+import socket
+import threading
+
+PORT = 7501
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDRESS = (SERVER, PORT)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDRESS) 
+
+def handle_client(connection, address):
+    print(f"[NEW CONNECTION] {address} connected.")
+    
+    connected = True
+    while connected:
+        message_length = connection.recv(16).decode('utf-8')
+        if message_length:
+            message_length = int(message_length)
+            message = connection.recv(message_length).decode('utf-8')
+            if message == "Disconnect":
+                connected = False
+        
+        print(f"[{address}] {message}")
+        connection.send("Message received".encode('utf-8'))
+    
+    connection.close()
