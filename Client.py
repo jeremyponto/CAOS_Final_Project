c0747ea94008b68fc3a356ae3d0e2a7e8b7f4491
kevinherman1104 <liemkevin1104@gmail.com>
2021-06-20 14:19:40 +0700

    added client to receive

--- Client.py
+++ Client.py
@@ -3,3 +3,19 @@ import socket
 PORT = 7501
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDRESS = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDRESS)
+
+def send(msg):
+    message = msg.encode('utf-8')
+    message_length = len(message)
+    send_length = str(message_length).encode('utf-8')
+    send_length += b" " * (16 - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode('utf-8'))
+
+while True:
+    sentMsg = input()
+    send(sentMsg)
